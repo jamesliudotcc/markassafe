35c0b97e4b52bd63e7b845d99898d2b6e4733adb
James Liu <james@jamesliu.cc>
2023-10-08 16:27:27 -0700

    Implement in memory

---
+++ app/repository/in_memory.py
@@ -0,0 +1,75 @@
+from collections import defaultdict
+
+from ..entity.entity import Entity
+from .abstract import AbstractRepository
+
+
+def filter_query(result: set[Entity] | list[Entity], entity_query: dict) -> list[dict]:
+    """Given a collection of results and an entity query, filter the results to only
+    those matching the query."""
+    return [
+        r for r in result if all([getattr(r, k) == v for k, v in entity_query.items()])
+    ]
+
+
+class InMemoryRepository(AbstractRepository):
+    """Database implemented as in-memory Python structures.
+
+    Use a defaultdict to store types. Each key in the defaultdict
+    contains a list of items.
+    """
+
+    def __init__(self):
+        self.delete_all()
+
+    def next_id(self, entity_type: str) -> int:
+        """Given an entity type, give the correct next auto-increment id."""
+        if not self.database[entity_type]:
+            return 1
+        return self.database[entity_type][-1].id + 1
+
+    def delete_all(self):
+        """Start over."""
+        self.database = defaultdict(list)
+
+    def add(self, entity_type: str, entity: Entity) -> Entity:
+        """Add an entity to the default dict."""
+        entity.id = self.next_id(entity_type)
+
+        self.database[entity_type].append(entity)
+
+        return entity
+
+    def get(
+        self,
+        entity_type,
+        entity_query: dict | None = None,
+        skip: int = 0,
+        stride: int = 10,
+    ):
+        """Get from the default dict."""
+        if entity_type not in self.database:
+            raise ValueError(f"Entity type {entity_type} does not exist.")
+
+        if entity_query is None:
+            entity_query = {}
+
+        results = self.database[entity_type]
+        filtered_results = filter_query(results, entity_query)
+
+        return filtered_results[skip : skip + stride]
+
+    def get_one(self, type, query):
+        return self.get(type, query, stride=1)[0]
+
+    def update(self, entity_type: str, entity_id: int, changes: dict | None) -> Entity:
+        """Changes the object in the database"""
+        match = self.get_one(entity_type, {"id": entity_id})
+
+        for k, v in changes.items():
+            setattr(match, k, v)
+
+        return match
+
+
+in_memory_repository = InMemoryRepository()
---
+++ app/repository/test_in_memory.py
@@ -0,0 +1,85 @@
+"""Test in-memory repository implementation."""
+
+from typing import Optional
+
+from ..entity.entity import Entity
+from .in_memory import filter_query, in_memory_repository
+
+
+class EntityWithId(Entity):
+    id: Optional[int]
+
+
+class AdventureLike(Entity):
+    id: Optional[int]
+    user_id: int
+    active: bool = True
+
+
+def test_filter_query_by_id_null_result():
+    """When filtering by id, if the id does not result, an empty set is returned."""
+    in_memory_repository.delete_all()
+    results = [EntityWithId(id=1), EntityWithId(id=2)]
+    assert filter_query(results, {"id": 3}) == []
+
+
+def test_filter_query_by_id_result_exists():
+    """When filtering by id, if the id exists in results, it is returned."""
+    in_memory_repository.delete_all()
+    results = [EntityWithId(id=1), EntityWithId(id=2)]
+    assert filter_query(results, {"id": 1}) == [EntityWithId(id=1)]
+
+
+def test_filter_query_by_user_id():
+    """When filtering by user id, if the user id exists in results, those are returned.
+    """
+    in_memory_repository.delete_all()
+    results = [
+        AdventureLike(id=1, user_id=1),
+        AdventureLike(id=2, user_id=1),
+        AdventureLike(id=3, user_id=2),
+    ]
+    assert filter_query(results, {"user_id": 1}) == [
+        AdventureLike(id=1, user_id=1),
+        AdventureLike(id=2, user_id=1),
+    ]
+
+
+def test_filter_query_by_user_id_and_status():
+    """When filtering by user id and active status, return the correct result"""
+    in_memory_repository.delete_all()
+    results = [
+        AdventureLike(id=1, user_id=1),
+        AdventureLike(id=2, user_id=1),
+        AdventureLike(id=3, user_id=1, active=False),
+        AdventureLike(id=4, user_id=2),
+    ]
+    assert filter_query(results, {"user_id": 1, "active": True}) == [
+        AdventureLike(id=1, user_id=1),
+        AdventureLike(id=2, user_id=1),
+    ]
+
+
+def test_add_auto_increments_id():
+    """This is only necessary becaues we are not using a real db"""
+    in_memory_repository.delete_all()
+    in_memory_repository.add("adventure_like", AdventureLike(user_id=1))
+    adventure_from_db = in_memory_repository.get("adventure_like")[-1]
+
+    assert adventure_from_db.id == 1
+
+    # And then do a second one.
+    in_memory_repository.add("adventure_like", AdventureLike(user_id=1))
+    adventure_from_db = in_memory_repository.get("adventure_like")[-1]
+
+    assert adventure_from_db.id == 2
+
+
+def test_update_adventure():
+    in_memory_repository.delete_all()
+    in_memory_repository.add("adventure_like", AdventureLike(user_id=1))
+    in_memory_repository.update("adventure_like", 1, {"active": False})
+
+    assert in_memory_repository.get("adventure_like", {"id": 1})[0] == AdventureLike(
+        id= 1, user_id=1, active=False
+    )
